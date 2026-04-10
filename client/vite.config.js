3b7311a4da4fc24dc57000d9c39d6ee8a246d721
Le-Thinh <lqt9112003@gmail.com>
2026-04-10 20:25:18 +0700

    fix url proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import dotenv from "dotenv";
+dotenv.config();
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    proxy: { "/v1/api": "http://localhost:3055" },
+    proxy: { "/v1/api": "https://ccloud-convert-dev.onrender.com" },
   },
   plugins: [react(), tailwindcss()],
 });
